bfc7372078a0fbef97303799811690ff2e409c1d
Leonard <fdavidliviu@yahoo.ro>
2025-05-03 21:13:00 +0300

    migr

---
+++ WinUIApp.Data/Migrations/20250503181150_UniqueConstraintuserdrink.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WinUIApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueConstraintuserdrink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Ratings_UserID",
+                table: "Ratings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_UserID_DrinkID",
+                table: "Ratings",
+                columns: new[] { "UserID", "DrinkID" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Ratings_UserID_DrinkID",
+                table: "Ratings");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_UserID",
+                table: "Ratings",
+                column: "UserID");
+        }
+    }
+}
